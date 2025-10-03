f69902f545013b7d10e6831d976be19ceb1949e7
DominicDev <s0l0estudi4r@gmail.com>
2025-10-03 15:50:53 +0200

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'HARRY STYLES';
+export let nameSongs = [
+	"WATERMELON SUGAR", /*1*/
+	"AS IT WAS", /*2*/
+	"SIGN OF THE TIMES", /*3*/
+	"TWO GHOSTS", /*4*/
+	"GOLDEN", /*5*/
+	"ONLY ANGEL", /*6*/
+	"CHERRY", /*7*/
+	"FINE LINE", /*8*/
+	"FALLING", /*9*/
+	"GIRL CRUSH", /*10*/
+	"MATILDA", /*11*/
+	"SWEET CREATURE", /*12*/
+	"EVER SINCE NEW YORK", /*13*/
+	"WOMAN", /*14*/
+	"CINEMA", /*15*/
+	"LITTLE FREAK", /*16*/
+	"MEET ME IN THE HALLWAY", /*17*/
+	" FROM THE DINING TABLE", /*18*/
+	"ADORE YOU", /*19*/
+	"SHE", /*20*/
+]
